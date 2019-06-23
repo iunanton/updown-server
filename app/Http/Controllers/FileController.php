59f94791125947f6686ec7b33296280769f863a8
Anton Iun <iun@edgeconsult.me>
2019-06-23 16:08:22 +0300

    Show files inline

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -123,7 +123,7 @@ class FileController extends Controller
         }
         return response()->streamDownload(function() use ($fileContent) {
             echo $fileContent;
-        }, $file->name);
+        }, $file->name, ['Content-Type' => $file->mime_type], 'inline');
     }
 
     /**
